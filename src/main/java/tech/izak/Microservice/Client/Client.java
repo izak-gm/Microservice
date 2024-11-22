38cf2784bc4a9002a6afc58e07c5092dadd7a0be
izak <maingiisaac27@gmail.com>
2024-11-22 10:02:19 +0300

    Spring boot User employee commit

--- src/main/java/tech/izak/Microservice/Client/Client.java
+++ src/main/java/tech/izak/Microservice/Client/Client.java
@@ -1,4 +1,22 @@
 package tech.izak.Microservice.Client;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Client {
+    @Id
+    @SequenceGenerator(
+            name =  "client_sequence",
+            sequenceName = "client_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "client_sequence"
+    )
+
+    private Integer id;
+    private String firstName;
+
 }
